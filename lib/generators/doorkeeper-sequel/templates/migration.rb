98e06c48089f5353cb71b898376a75f8340ffbc7
Nikita Bulaj <bulaj.nikita@gmail.com>
2016-04-20 18:29:12 +0300

    fix migration template

--- lib/generators/doorkeeper-sequel/templates/migration.rb
+++ lib/generators/doorkeeper-sequel/templates/migration.rb
@@ -1,35 +1,34 @@
 Sequel.migration do
   change do
-    # TODO: double check types, keys and indexes
     create_table :oauth_applications do
       primary_key :id
 
-      column :name, :string, null: false
-      column :uid, :string, null: false, index: { unique: true }
-      column :secret, :string, null: false
+      column :name, String, size: 255, null: false
+      column :uid, String, size: 255, null: false, index: { unique: true }
+      column :secret, String, size: 255, null: false
 
-      column :scopes, :string, null: false, default: ''
-      column :redirect_uri, :string, text: true
+      column :scopes, String, size: 255, null: false, default: ''
+      column :redirect_uri, String
 
       column :created_at, DateTime
       column :updated_at, DateTime
     end
 
     create_table :oauth_access_grants do
-      column :resource_owner_id, :integer, null: false
+      column :resource_owner_id, Integer, null: false
 
       foreign_key :application_id, :oauth_applications, null: false, on_delete: :cascade
 
-      column :token, :string, null: false, index: { unique: true }
-      column :expires_in, :integer, null: false
-      column :redirect_uri, :string, text: true, null: false
+      column :token, String, size: 255, null: false, index: { unique: true }
+      column :expires_in, Integer, null: false
+      column :redirect_uri, String, null: false
       column :created_at, DateTime, null: false
       column :revoked_at, DateTime
-      column :scopes, :string
+      column :scopes, String, size: 255,
     end
 
     create_table :oauth_access_tokens do
-      column :resource_owner_id, :integer, index: true
+      column :resource_owner_id, Integer, index: true
 
       foreign_key :application_id, :oauth_applications, null: false, on_delete: :cascade
 
@@ -38,14 +37,14 @@ Sequel.migration do
       # characters. More info on custom token generators in:
       # https://github.com/doorkeeper-gem/doorkeeper/tree/v3.0.0.rc1#custom-access-token-generator
       #
-      # column :token, :string, text: true, null: false
-      column :token, :string, null: false, index: { unique: true }
+      # column :token, String, null: false
+      column :token, String, size: 255, null: false, index: { unique: true }
 
-      column :refresh_token, :string, index: { unique: true }
-      column :expires_in, :integer
+      column :refresh_token, String, size: 255, index: { unique: true }
+      column :expires_in, Integer
       column :revoked_at, DateTime
       column :created_at, DateTime, null: false
-      column :scopes, :string
+      column :scopes, String, size: 255,
     end
   end
 end
