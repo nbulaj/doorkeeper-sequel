c40337f8e8916a627311146c093329cfbf382636
Gencer W. Genç <gencer@users.noreply.github.com>
2018-10-28 16:05:32 +0300

    :code_challenge is string not integer.

--- lib/doorkeeper-sequel/generators/templates/enable_pkce_migration.rb
+++ lib/doorkeeper-sequel/generators/templates/enable_pkce_migration.rb
@@ -1,7 +1,7 @@
 Sequel.migration do
   change do
     alter_table(:oauth_access_grants) do
-      add_column :code_challenge, Integer, null: true
+      add_column :code_challenge, String, null: true
       add_column :code_challenge_method, String, null: true
     end
   end
